a38a10dceab565a6902279a61bfd539ed51f9fce
DiegooMBB <128840164+DiegooMBB@users.noreply.github.com>
2023-03-27 21:05:43 -0300

    Update questao2.py

--- questao2.py
+++ questao2.py
@@ -1,10 +1,8 @@
-print('=========Questão 3=========')                                    #QUESTÃO 3
-num1=int(input('Digite um valor: '))                                    #QUESTÃO 3
-num2=int(input('Digite outro valor: '))                                 #QUESTÃO 3
-if num1 != num2:                                                        #QUESTÃO 3
-    if num1 > num2:                                                     #QUESTÃO 3
-        print('O valor', num1,' é o maior.')                            #QUESTÃO 3
-    else:                                                               #QUESTÃO 3
-        print('O valor', num2,' é o maior.')                            #QUESTÃO 3
-else:                                                                   #QUESTÃO 3
-    print('Reinicie o programa e digite numeros diferentes.')           #QUESTÃO 3
+print('=================QUESTÃO 2=================')
+n1=float(input('Digite o valor da primeira nota: '))
+n2=float(input('Digite o valor da segunda nota: '))
+md=float((n1+n2)/2)
+if md>=6:
+    print('O aluno teve uma media %.2f e foi aprovado.'%md)
+else:
+    print('O aluno teve uma media %.1f e foi reprovado.'%md)
