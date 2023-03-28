ba7583239de7081d79b58312a793f398f16f3d36
DiegooMBB <128840164+DiegooMBB@users.noreply.github.com>
2023-03-27 21:00:10 -0300

    Update questao5.py

--- questao5.py
+++ questao5.py
@@ -1,10 +1,12 @@
-print('=====================ESTOQUE TURMA P1 A=====================')
-EstAt=int(input('Digite a quantidade atual da mercadoria no estoque: '))
-QuantMAX=int(input('Digite a quantidade máxima de mercadorias no estoque: '))
-QuantMIN=int(input('Digite a quantidade mínima de mercadorias no estoque: '))
-QuantMEDIA=(QuantMAX+QuantMIN)/2
-print('=====================PREPARANDO VALORES=====================')
-if EstAt>=QuantMEDIA:
-    print('Não efetue esta compra.')
-else:
-    print('Pode efetuar esta compra.')
+print('=====================BANCO TURMA P1 A=====================')         #QUESTÃO 5
+cc=int(input('Digite o numero da sua conta: '))                             #QUESTÃO 5
+SalAnt=float(input('Digite o saldo anterior: '))                            #QUESTÃO 5
+ValDeb=float(input('Valor do débito: '))                                    #QUESTÃO 5
+ValCred=float(input('Valor do saldo em crédito: '))                         #QUESTÃO 5
+print('======================SALDO DA CONTA======================')         #QUESTÃO 5
+NovoSal= SalAnt - ValDeb + ValCred                                          #QUESTÃO 5
+print('O seu saldo atual é de: {:.2f}'.format(NovoSal),'reais.')            #QUESTÃO 5
+if NovoSal >= 0:                                                            #QUESTÃO 5
+    print('O seu saldo atual é POSITIVO.')                                  #QUESTÃO 5
+else:                                                                       #QUESTÃO 5
+    print('O seu saldo atual é NEGATIVO.')                                  #QUESTÃO 5
